c61e6c49371074e313832f4327e45228d06db83a
Reinier The Ghost <reinierrolbrouwer@gmail.com>
2025-04-11 20:30:55 +0200

    started porting worldgen

--- src/main/java/com/legends/edumia/world/congiguredfeatures/crystals/CrystalConfiguresFeatures.java
+++ src/main/java/com/legends/edumia/world/congiguredfeatures/crystals/CrystalConfiguresFeatures.java
@@ -5,7 +5,7 @@ import com.legends.edumia.core.BlockLoader;
 import com.legends.edumia.world.features.EdumiaFeatures;
 import com.legends.edumia.world.features.crystal.CrystalFeatureConfig;
 import net.minecraft.core.registries.Registries;
-import net.minecraft.data.worldgen.BootstapContext;
+import net.minecraft.data.worldgen.BootstrapContext;
 import net.minecraft.resources.ResourceKey;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.level.levelgen.feature.ConfiguredFeature;
@@ -16,17 +16,17 @@ import net.minecraft.world.level.levelgen.feature.stateproviders.BlockStateProvi
 public class CrystalConfiguresFeatures {
 
     public static final ResourceKey<ConfiguredFeature<?, ?>> ELVEN_CRYSTAL = registerKey("elven_crystal");
-    public static void bootstrap(BootstapContext<ConfiguredFeature<?, ?>> featureRegisterable) {
+    public static void bootstrap(BootstrapContext<ConfiguredFeature<?, ?>> featureRegisterable) {
         register(featureRegisterable, ELVEN_CRYSTAL, EdumiaFeatures.CRYSTAL.get(),
                 new CrystalFeatureConfig(BlockStateProvider.simple(BlockLoader.HIGH_ELVEN_CRYSTAL.get()), 64,6, 4, 6));
 
     }
 
     public static ResourceKey<ConfiguredFeature<?, ?>> registerKey(String name){
-        return ResourceKey.create(Registries.CONFIGURED_FEATURE, new ResourceLocation(Edumia.MOD_ID, "crystals/" + name));
+        return ResourceKey.create(Registries.CONFIGURED_FEATURE, ResourceLocation.fromNamespaceAndPath(Edumia.MOD_ID, "crystals/" + name));
     }
 
-    private static <FC extends FeatureConfiguration, F extends Feature<FC>> void register(BootstapContext< ConfiguredFeature<?, ?>> context,
+    private static <FC extends FeatureConfiguration, F extends Feature<FC>> void register(BootstrapContext< ConfiguredFeature<?, ?>> context,
                                                                                           ResourceKey<ConfiguredFeature<?, ?>> key, F feature,
                                                                                           FC configuration){
         context.register(key, new ConfiguredFeature<>(feature, configuration));
